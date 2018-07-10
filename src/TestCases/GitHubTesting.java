3d704ee9489e5fdedc72fec8794048e115471158
gunj22 <sam_bhasin4@yahoo.com>
2018-07-10 18:10:31 +0530

    GitHubTesting--- SYSO command remove and one new File SumitLocalNewClass is added

--- src/TestCases/GitHubTesting.java
+++ src/TestCases/GitHubTesting.java
@@ -5,7 +5,9 @@ public class GitHubTesting {
 	public static void main(String[] args) {
 		
 		System.out.println("Git Hub Testing is workign fine");
-		System.out.println("Second time  Git Hub Testing is workign fine");
+		
+		
+		
 		
 	}
 
